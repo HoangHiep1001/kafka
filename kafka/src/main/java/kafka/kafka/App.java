623bac5da4611331c0799ed56199b66e69ab74bc
HoangHiep1001 <hiephm1001@gmail.com>
2019-10-05 10:41:27 +0700

    aa

--- kafka/src/main/java/kafka/kafka/App.java
+++ kafka/src/main/java/kafka/kafka/App.java
@@ -1,13 +1,72 @@
 package kafka.kafka;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class App {
+
+	private final static String TOPIC = "test";
+	private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+//private final static String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
+
+	public static void main(String... args) throws Exception {
+		runConsumer();
+	}
+
+	private static Consumer<Long, String> createConsumer() {
+
+		final Properties props = new Properties();
+
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+// The maximum records returned by the poll()
+//props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100);
+
+// Create the consumer using props.
+		final Consumer<Long, String> consumer = new KafkaConsumer<Long, String>(props);
+
+// Subscribe to the topic.
+		consumer.subscribe(Collections.singletonList(TOPIC));
+
+		return consumer;
+	}
+
+	static void runConsumer() throws InterruptedException {
+
+		final Consumer<Long, String> consumer = createConsumer();
+
+		final int giveUp = 100;
+		int noRecordsCount = 0;
+
+		while (true) {
+
+			final ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
+
+			if (consumerRecords.count() == 0) {
+				noRecordsCount++;
+				if (noRecordsCount > giveUp)
+					break;
+				else
+					continue;
+			}
+
+			consumerRecords.forEach(record -> {
+				System.out.printf("Consumer Record:(%d, %s, %d, %d)\n", record.key(), record.value(),
+						record.partition(), record.offset());
+			});
+
+			consumer.commitAsync();
+		}
+
+		consumer.close();
+		System.out.println("DONE");
+	}
 }
